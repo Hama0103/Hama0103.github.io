e8cec8588d5c352933009bc8c6c5a436a4fca3fe
浜田紀 <nori@hama.local>
2020-03-29 03:01:16 +0900

    add section and scroll

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
+
 window.onload = function () {
-    
 }
 
-function Workclick(){
-    target = document.getElementById("panel");
-    if(target.style.display == "none"){
-        target.style.display = "block";
-    }else{
-        target.style.display = "none"
-    }
-}
+$(function(){
+    $('a[href^="#"]').click(function(){
+        var speed = 500;
+        var href= $(this).attr("href");
+        var target = $(href == "#" || href == "" ? 'html' : href);
+        var position = target.offset().top - 70;
+        $("html, body").animate({scrollTop:position}, speed, "swing");
+        return false;
+    });
+});
