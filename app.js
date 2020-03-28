0457fdacbb5b20aedb033c4fbaec107fdbc2cc55
浜田紀 <nori@hama.local>
2020-03-28 21:45:56 +0900

    add section

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
 window.onload = function () {
     
 }
+
+function Workclick(){
+    target = document.getElementById("panel");
+    if(target.style.display == "none"){
+        target.style.display = "block";
+    }else{
+        target.style.display = "none"
+    }
+}
